56307aff57549f45ada263e69c89f552a12e7070
arily <lbsaaaaa@gmail.com>
2020-01-31 23:27:57 +0900

    久等了

--- exsperDoOrNot.js
+++ exsperDoOrNot.js
@@ -25,7 +25,7 @@ ReplyObject.prototype.toString = function() {
         return undefined;
     }
 }
-ReplyObject.prototype.no = function(){
+ReplyObject.prototype.no = function() {
     this.reply = false;
     return this;
 }
@@ -58,15 +58,9 @@ function LookForTheSame(s1, s2) {
  * @param {string} s 接受的消息
  * @return {ReplyObject} 所有可选项
  */
-function Reply(s) {
-    const reply = new ReplyObject(s);
-    // 排除不含“不”的消息
-    // 排除含有“不不”的消息（应该不是询问选择的）
-    if (!s.includes("不") || s.includes("不不")) return reply.no();
-    // 排除过长和过短消息
-    if (s.length > 30 || s.length < 4) return reply.no();
+function AorB(ask) {
+    const reply = new ReplyObject(ask);
 
-    const ask = s.substring(1);
     let asklength = ask.length;
 
     // 获取所有“不”的位置
@@ -131,10 +125,63 @@ function Reply(s) {
     // 将“我”改成“你”，“你”改成“我”
     let replyStringFix = replyString.split("").map(char => (char == '我') ? '你' : (char == '你' ? '我' : char)).join("");
 
-    reply.choices = [replyStringFix,`不${replyStringFix}`]
+    reply.choices = [replyStringFix, `不${replyStringFix}`]
     return reply
 }
 
+function DidOrNot(ask) {
+    const reply = new ReplyObject(ask);
+    const singleWordAction = ["咬","吞","吐","吮","吸","啃","喝","吃","咀","嚼","噘","嘟","努","撇","看","望","瞥","视","盯","瞧","窥","瞄","眺","瞪","瞅","搀","抱","搂","扶","捉","擒","掐","推","拿","抽","撕","摘","拣","捡","打","播","击","捏","撒","按","弹","撞","提","扭","捶","持","揍","披","捣","搜","托","举","拖","擦","敲","挖","抛","掘","抬","插","扔","写","抄","抓","捧","掷","撑","摊","倒","摔","劈","画","搔","撬","挥","揽","挡","捺","抚","搡","拉","摸","拍","摇","剪","拎","拔","拧","拨","舞","握","攥","退","进","奔","跑","赶","趋","遁","逃","立","站","跨","踢","跳","走","蹬","窜","说","看","走","听","笑","拿","跑","吃","唱","喝","敲","坐","盯","踢","闻","摸","在","死","有","想","爱","恨","伯","是","为","乃","能","会","愿","肯","敢","要","配","上","下","进","出","回","开","过","起","来"]
+    const action = ask.match(/(?<action>.+)了[嘛吗](\?)?/);
+    console.log(action);
+    if (action.groups.action.length == 2 && singleWordAction.includes(action.groups.action.slice(0,1))) reply.choices = [`${action.groups.action.slice(0,1)}了`,`没${action.groups.action.slice(0,1)}`];
+    else if (action.groups.action.length == 2) reply.choices = [`${action.groups.action}了`,`没${action.groups.action}`];
+    else reply.choices = [`${action.groups.action}了`,`没有`];
+    return reply
+}
+
+function findBuilder() {
+    this.finder = [{
+            matcher: (s) => (s.includes("不") && !s.includes("不不") && s.length <= 30 && s.length > 4),
+            builder: AorB
+        },
+        {
+            matcher: (s) => s.match(/(.+)了[嘛吗](\?)?/),
+            builder: DidOrNot
+        }
+    ];
+}
+findBuilder.prototype.returnBuilderIfMatched = function(s) {
+    const match = this.finder.find(matcher => matcher.matcher(s))
+    if (match !== undefined) {
+        return match.builder;
+    } else {
+        return false;
+    }
+}
+let readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+let b = new findBuilder();
+rl.on('line', function(line) {
+    let ask = line;
+    if (ask.substring(0, 1) === "!" || ask.substring(0, 1) === "！") {
+        try {
+            let str = ask.trim().substring(1);
+            let replyString = b.returnBuilderIfMatched(str);
+            console.log(replyString);
+            if (replyString) {
+                return console.log(replyString(str).toString());
+            }
+        } catch (ex) {
+            console.log(ex);
+        }
+    }
+});
 // Koishi插件名
 module.exports.name = 'exsperDoOrNot';
 
@@ -144,7 +191,7 @@ module.exports.apply = (ctx) => {
         let ask = meta.message;
         if (ask.substring(0, 1) === "!" || ask.substring(0, 1) === "！") {
             try {
-                let replyString = Reply(ask.trim());
+                let replyString = Reply(ask.trim().substring(1));
                 if (!replyString.reply) return next();
                 else return meta.$send(replyString.toString());
             } catch (ex) {
